9552767b31c3747592f3ad26354156eec514dda3
chilledornaments <mitch@chilledornaments.com>
2022-09-01 09:48:28 -0600

    fix force logic,

--- script.py
+++ script.py
@@ -12,6 +12,8 @@ import queue
 TFC_BASE_URL = "https://app.terraform.io/api/v2"
 ORGANIZATION_NAME = os.environ.get("TFC_ORG_NAME", "Nutrien")
 
+# TODO - build constants to hold OK, failed, etc statuses
+
 
 argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument("--prefix", dest="prefix", action="store", type=str)
@@ -99,15 +101,22 @@ def get_workspaces_by_prefix(session: requests.sessions.Session, prefix):
     return workspaces
 
 
-def get_workspace_latest_run_id(session: requests.sessions.session, name: str):
-    # https://www.terraform.io/cloud-docs/api-docs/workspaces#show-workspace
+def get_workspace_latest_run_id(
+    session: requests.sessions.session, workspace_id: str, workspace_name: str
+):
+    # https://www.terraform.io/cloud-docs/api-docs/run#list-runs-in-a-workspace
     resp = session.get(
-        f"{TFC_BASE_URL}/organizations/{ORGANIZATION_NAME}/workspaces/{name}"
+        f"{TFC_BASE_URL}/workspaces/{workspace_id}/runs", params={"page[size]": 1}
     )
 
     j = json.loads(resp.text)
 
-    return j["data"]["relationships"]["latest-run"]["data"]["id"]
+    if len(j["data"]) == 0:
+        print(f"no data for workspace '{workspace_name}")
+        print(j)
+        return None
+
+    return j["data"][0]["id"]
 
 
 def get_workspace_current_run_id(session: requests.sessions.session, name: str):
@@ -129,7 +138,63 @@ def can_apply_plan(session: requests.sessions.session, plan_id: str):
     return j["data"]["attributes"]["actions"]["is-confirmable"]
 
 
-# def force_execute_run(session): pass
+def force_execute_run(session: requests.sessions.session, run_id: str) -> bool:
+    # https://www.terraform.io/cloud-docs/api-docs/run#forcefully-execute-a-run
+
+    try:
+        # First, make sure we actually can force-execute this plan
+        print(f"checking to see if we can force execute '{run_id}'")
+
+        resp = session.get(f"{TFC_BASE_URL}/runs/{run_id}")
+        # If the run is pending then it's sitting in the queue waiting for another run to finish,
+        # which means we can force execute it
+        status = json.loads(resp.text)["data"]["attributes"]["status"]
+        if status == "pending":
+            print(f"run '{run_id}' can be force executed, doing so now")
+
+            resp = session.post(
+                f"{TFC_BASE_URL}/runs/{run_id}/actions/force-execute",
+                headers={"Content-Type": "application/vnd.api+json"},
+            )
+            print(resp.text)
+            time.sleep(5)
+
+            counter = 0
+            done = False
+
+            # TODO - variable this
+            while counter < 10 and not done:
+                print(f"checking status of forcefully executed run '{run_id}'")
+                resp = session.get(f"{TFC_BASE_URL}/runs/{run_id}")
+
+                status = json.loads(resp.text)["data"]["attributes"]["status"]
+
+                if status in ["planned", "planned_and_finished"]:
+                    done = True
+                    print(f"successfully planned run '{run_id}'")
+                elif status in ["errored", "canceled", "discarded", "force_canceled"]:
+                    # done = True
+                    print(f"run '{run_id}' failed with status '{status}'")
+                    return False
+                else:
+                    print(
+                        f"run '{run_id}' is '{status}'. checking again in 5 seconds (total checks: {counter}, max checks: {10})"
+                    )
+
+                counter += 1
+
+                time.sleep(5)
+
+            return True
+        elif status == "planned":
+            print(f"latest run is already planned for workspace '{workspace_name}'")
+            return True
+        else:
+            print(f"run {run_id} is '{status}', not 'pending', cannot force execute")
+    except Exception as e:
+        print(f"exception while force executing run - {e}")
+
+    return False
 
 
 def apply_plan(
@@ -143,61 +208,75 @@ def apply_plan(
         try:
             print("pulling new run from queue")
 
-            run_id = run_queue.get(block=True, timeout=1)
+            run_object = run_queue.get(block=True, timeout=1)
 
-            print(f"pulled '{run_id}' off queue")
+            run_id = run_object["id"]
+            force = run_object["force"]
 
-            resp = session.post(
-                f"{TFC_BASE_URL}/runs/{run_id}/actions/apply",
-                headers={"Content-Type": "application/vnd.api+json"},
-            )
+            print(f"pulled '{run_id}' off queue (force: {force})")
 
-            if resp.text != "null":
-                print(f"resp.text is {resp.text}")
-                if not json.loads(resp.text).get("success", False):
-                    print(f"error queuing apply - {resp.text}")
+            proceed = True
+            if force:
+                if not force_execute_run(tfc_session, run_id):
+                    print(f"force execute unsuccesful for workspace '{workspace_name}'")
+                    proceed = False
 
-                    raise Exception("error")
+            if proceed:
+                resp = session.post(
+                    f"{TFC_BASE_URL}/runs/{run_id}/actions/apply",
+                    headers={"Content-Type": "application/vnd.api+json"},
+                )
 
-            print(f"queued apply for run '{run_id}'")
+                if resp.text != "null":
+                    print(f"resp.text is {resp.text}")
+                    if not json.loads(resp.text).get("success", False):
+                        print(f"error queuing apply - {resp.text}")
 
-            # Wait for terraform cloud to do stuff
-            time.sleep(2)
+                        raise Exception("error")
 
-            counter = 0
-            done = False
-            # TODO - handle success notification better
+                print(f"queued apply for run '{run_id}'")
 
-            # TODO - max counter configurable
-            while counter < max_checks and not done:
-                resp = session.get(f"{TFC_BASE_URL}/runs/{run_id}")
+                # Wait for terraform cloud to do stuff
+                time.sleep(2)
 
-                status = json.loads(resp.text)["data"]["attributes"]["status"]
+                counter = 0
+                done = False
+                # TODO - handle success notification better
 
-                if status == "applied":
-                    done = True
-                    print(f"successfully applied run '{run_id}'")
-                elif status == "errored":
-                    done = True
-                    print(f"failed to apply run '{run_id}'")
-                else:
-                    print(
-                        f"run '{run_id}' is '{status}'. checking again in 5 seconds (total checks: {counter}, max checks: {max_checks})"
-                    )
+                # TODO - max counter configurable
+                while counter < max_checks and not done:
+                    resp = session.get(f"{TFC_BASE_URL}/runs/{run_id}")
 
-                counter += 1
+                    status = json.loads(resp.text)["data"]["attributes"]["status"]
 
-                time.sleep(5)
+                    if status == "applied":
+                        done = True
+                        print(f"successfully applied run '{run_id}'")
+                    elif status == "errored":
+                        done = True
+                        print(f"failed to apply run '{run_id}'")
+                    else:
+                        print(
+                            f"run '{run_id}' is '{status}'. checking again in 5 seconds (total checks: {counter}, max checks: {max_checks})"
+                        )
 
-            run_queue.task_done()
+                    counter += 1
 
+                    time.sleep(5)
+
+                run_queue.task_done()
+            else:
+                print(
+                    f"run '{run_id}' encountered an issue, marking item done and moving on"
+                )
+                run_queue.task_done()
         except queue.Empty:
             print("queue is empty")
             break
 
         except Exception as e:
             print(f"exception - {e}")
-            break
+            return
 
 
 parsed_args = argument_parser.parse_args()
@@ -217,19 +296,29 @@ apply_queue = queue.Queue()
 
 for workspace_name, workspace_id in workspaces.items():
     if parsed_args.force:
-        run_id = get_workspace_latest_run_id(tfc_session, workspace_name)
+        print(f"force is set, retrieving latest run for workspace {workspace_name}")
+        run_id = get_workspace_latest_run_id(tfc_session, workspace_id, workspace_name)
+        queue_item = {"id": run_id, "force": True}
+        apply_queue.put(queue_item, block=False)
+        print(
+            f"put item on queue for '{workspace_name}' - this run is forced and may produce unexpected behavior"
+        )
+
     else:
         run_id = get_workspace_current_run_id(tfc_session, workspace_name)
+        queue_item = {"id": run_id, "force": False}
 
-    if can_apply_plan(tfc_session, run_id):
-        apply_queue.put(run_id, block=False)
+        if can_apply_plan(tfc_session, run_id):
+            apply_queue.put(queue_item, block=False)
 
-        print(f"put item on queue for '{workspace_name}'")
-    else:
-        print(f"unable to apply run for workspace '{workspace_name}'")
+            print(f"put item on queue for '{workspace_name}'")
+        else:
+            print(f"unable to apply run for workspace '{workspace_name}'")
 
-    if apply_queue.qsize() == 0:
-        print("no items to apply")
+
+if apply_queue.qsize() == 0:
+    print("no items to apply")
+    sys.exit(0)
 
 print("workspaces to apply:")
 for w in workspaces.keys():
