e6a397e7f07817beda145cff19dd68fd078ab69b
chilledornaments <mitch@chilledornaments.com>
2025-09-18 07:26:56 -0600

    chore: delete old python code

--- legacy/tfc_tool/helpers.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-def prompt_for_input():
-    v = input("\n\n ** apply: [y/N] ")
-
-    if v not in ["y", "Y"]:
-        raise Exception(f"received '{v}', expecting 'y' or 'Y' - exiting")
